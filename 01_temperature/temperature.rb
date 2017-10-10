bb8a9be4b79a4d6f90e80624ee3b9806470a63d0
Arnaud AGBO <agbo.arnaud@yahoo.fr>
2017-10-10 14:48:43 +0200

    exercice 0 et 1

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(fahrenheit)
+    celcius = (fahrenheit - 32) / 1.8
+    celcius.round
+end 
+
+def ctof(celcius)
+    fahrenheit = (celcius * 1.8) + 32
+    fahrenheit
+end
